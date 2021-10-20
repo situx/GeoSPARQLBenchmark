80b2688559894b2cb6d1f0c1410ca9d9bbef8369
Timo <situx@users.noreply.github.com>
2021-10-20 14:20:05 +0200

    Changed docker generation to new names

--- prepare_dockers.sh
+++ prepare_dockers.sh
@@ -2,12 +2,12 @@
 
 mvn package
 
-docker build -f docker/gsb-benchmarkcontroller.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-benchmarkcontroller .
-docker build -f docker/gsb-datagenerator.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-datagenerator .
-docker build -f docker/gsb-seqtaskgenerator.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-seqtaskgenerator .
-docker build -f docker/gsb-evaluationmodule.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-evaluationmodule .
+docker build -f docker/gsb-benchmarkcontroller.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-benchmarkcontroller-v2 .
+docker build -f docker/gsb-datagenerator.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-datagenerator-v2 .
+docker build -f docker/gsb-seqtaskgenerator.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-seqtaskgenerator-v2 .
+docker build -f docker/gsb-evaluationmodule.docker -t git.project-hobbit.eu:4567/mjovanovik/gsb-evaluationmodule-v2 .
 
-docker push git.project-hobbit.eu:4567/mjovanovik/gsb-benchmarkcontroller
-docker push git.project-hobbit.eu:4567/mjovanovik/gsb-datagenerator
-docker push git.project-hobbit.eu:4567/mjovanovik/gsb-seqtaskgenerator
-docker push git.project-hobbit.eu:4567/mjovanovik/gsb-evaluationmodule
+docker push git.project-hobbit.eu:4567/mjovanovik/gsb-benchmarkcontroller-v2
+docker push git.project-hobbit.eu:4567/mjovanovik/gsb-datagenerator-v2
+docker push git.project-hobbit.eu:4567/mjovanovik/gsb-seqtaskgenerator-v2
+docker push git.project-hobbit.eu:4567/mjovanovik/gsb-evaluationmodule-v2
